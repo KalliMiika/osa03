cce89d90843fb70f1e60a5fab2c68745d0c25767
KalliMiika <miika.kallioinen@helsinki.fi>
2020-02-10 17:29:30 +0200

    buildin lisäys

---
+++ build/precache-manifest.7d1e1e445b411c237b809e128c6cbb0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6884c38cd77c1c41281f355807c59deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b79e29a26eb64830720",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f714891d62a65a7a7260",
+    "url": "/static/js/2.a8034132.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a8034132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b79e29a26eb64830720",
+    "url": "/static/js/main.8fd7cd26.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
